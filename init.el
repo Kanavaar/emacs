d9ed7b1615e720f46541daa295a018e9c1595c27
Kanavaar <uju8765@gmail.com>
2023-01-15 21:16:44 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,11 @@
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+(load-theme 'wombat)
+;;; load custom theme (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
