9631a3ee145c0f9b22c0a70d712fc0f8fab4ee45
kanavaar <uju8765@gmail.com>
2023-03-03 23:07:07 +0100

    switching to vertico

--- init.el
+++ init.el
@@ -3,21 +3,26 @@
 (tool-bar-mode -1) ;; disable tool bar
 (tooltip-mode -1) ;; disable tooltip
 (set-fringe-mode 10) ;;set fringe to 10
-;; (menu-bar-mode -1) ; disable menu bar
+(menu-bar-mode -1) ; disable menu bar
 
 (global-display-line-numbers-mode 1) ; enable line numbers
 (setq display-line-numbers-type 'relative)
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Keymap stuff
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;(load-theme 'wombat)
-(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
-(load-theme 'kanagawa t)
+;; Themes
+;; (load-theme 'wombat)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
-;; Font Stuff
-(set-face-attribute 'default nil :font "Iosevka" :height 110)
+;; Font
+(set-face-attribute 'default nil :font "CaskaydiaCove Nerd Font" :height 95)
 
+;; Plugins
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -35,170 +40,128 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-; Magit
-(use-package magit)
-(require 'magit)
+;; Completion
 
-; Packages
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode))
 
-; Themes
-; Autothemer
-(use-package autothemer)
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless)))
 
-;; Doom Themes
+(use-package consult
+  :ensure t
+  :bind (
+	 ("C-x b" . consult-buffer)
+	 ))
+
+(use-package marginalia
+  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
+;; Modal editing
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-i-jump nil)
+  :bind (:map evil-normal-state-map
+	      ("SPC f f" . find-file)
+              ("SPC f w" . consult-ripgrep)
+              ("SPC b b" . consult-buffer)
+              ("SPC w" . evil-write)
+              ("SPC q" . evil-quit)
+              ("SPC SPC" . execute-extended-command))
+              ("C-h" . evil-window-left	)
+              ("C-j" . evil-window-down	)
+              ("C-k" . evil-window-up	)
+              ("C-l" . evil-window-right)
+  :config
+  (evil-mode 1)
+  (evil-set-undo-system 'undo-redo))
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; Colored delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Looks
+
+;; Nano theme
+(use-package nano-theme
+  :ensure t)
+
+;; Doom themes
 (use-package doom-themes
   :ensure t
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;(load-theme 'doom-one t)
+  (load-theme 'doom-opera-light t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  ;(setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
-;; Doom Modeline
+;; Modeline
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
-(use-package compat)
 
-;; Evil Mode
-(use-package evil)
-(require 'evil)
-(evil-mode 1)
+;; Font
+(use-package all-the-icons
+  :ensure t)
 
-; Ivy completion
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
 
-; Counsel
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil))
-
-;; Lsp Stuff
-
-;; Lsp-Mode
-(use-package lsp-mode
-  :ensure
-  :commands lsp
-  :custom
-  ;; what to use when checking on-save. "check" is default, I prefer clippy
-  (lsp-rust-analyzer-cargo-watch-command "clippy")
-  ;;(lsp-eldoc-render-all t)
-  (lsp-idle-delay 0.6)
-  ;; enable / disable the hints as you prefer:
-  (lsp-rust-analyzer-server-display-inlay-hints t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
-  (lsp-rust-analyzer-display-chaining-hints t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
-  (lsp-rust-analyzer-display-closure-return-type-hints t)
-  (lsp-rust-analyzer-display-parameter-hints nil)
-  (lsp-rust-analyzer-display-reborrow-hints nil))
-  ;;:config
-  ;;(add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-;; (use-package lsp-ui
-;;   :ensure
-;;   :commands lsp-ui-mode
-;;   :custom
-;;   (lsp-ui-peek-always-show t)
-;;   (lsp-ui-sideline-show-hover t)
-;;   (lsp-ui-doc-enable nil))
-
-;; Flycheck -- inline error checking
-(use-package flycheck :ensure)
-
-;; Company-mode
-(use-package company
-  :ensure
-  :custom
-  (company-idle-delay 0.2) ;; how long to wait until popup
-  ;; (company-begin-commands nil) ;; uncomment to disable popup
-  :bind
-  (:map company-active-map
-	      ("C-n". company-select-next)
-	      ("C-p". company-select-previous)
-	      ("M-<". company-select-first)
-	      ("M->". company-select-last)))
-
-(use-package yasnippet
-  :ensure
-  :config
-  (yas-reload-all)
-  (add-hook 'prog-mode-hook 'yas-minor-mode)
-  (add-hook 'text-mode-hook 'yas-minor-mode))
-
-;; Rust
-(use-package rustic
-  :ensure
-  :bind (:map rustic-mode-map
-              ("M-j" . lsp-ui-imenu)
-              ("M-?" . lsp-find-references)
-              ("C-c C-c l" . flycheck-list-errors)
-              ("C-c C-c a" . lsp-execute-code-action)
-              ("C-c C-c r" . lsp-rename)
-              ("C-c C-c q" . lsp-workspace-restart)
-              ("C-c C-c Q" . lsp-workspace-shutdown)
-              ("C-c C-c s" . lsp-rust-analyzer-status))
-  :config
-  ;; uncomment for less flashiness
-  (setq lsp-eldoc-hook nil)
-  (setq lsp-enable-symbol-highlighting nil)
-  (setq lsp-signature-auto-activate nil)
-
-  ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t)
-  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
-
-(defun rk/rustic-mode-hook ()
-  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
-  ;; save rust buffers that are not file visiting. Once
-  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
-  ;; no longer be necessary.
-  (when buffer-file-name
-    (setq-local buffer-save-without-query t))
-  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
-
-
+;; Keybindings
+;;(define-key evil-normal-state-map (kbd "SPC f f") 'find-file)
+;;(define-key evil-normal-state-map (kbd "SPC f w") 'consult-ripgrep)
+;;(define-key evil-normal-state-map (kbd "SPC b b") 'consult-buffer)
+;;(define-key evil-normal-state-map (kbd "SPC w") 'evil-write)
+;;(define-key evil-normal-state-map (kbd "SPC q") 'evil-quit)
+;;(define-key evil-normal-state-map (kbd "SPC SPC") 'execute-extended-command)
+;;(global-set-key (kbd "C-h") 'evil-window-left)
+;;(global-set-key (kbd "C-j") 'evil-window-down)
+;;(global-set-key (kbd "C-k") 'evil-window-up)
+;;(global-set-key (kbd "C-l") 'evil-window-right)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "feebf7a5a2675249a959372c255967711bda16954c017626aca17245e7836160" default))
- '(delete-selection-mode nil)
+   '("afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
  '(package-selected-packages
-   '(lsp-ui lsp-mode rustic magit evil compat counsel ivy doom-modeline autothemer use-package)))
+   '(evil-collection nano-theme rainbow-delimiters all-the-icons-dired vertico use-package solo-jazz-theme orderless marginalia evil doom-themes doom-modeline consult all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
