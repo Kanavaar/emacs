acca16a6d1ee1e19cc6469feac44622f0bd4e98f
kanavaar <uju8765@gmail.com>
2023-03-05 01:13:17 +0100

    adding general keybinds

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;; Variables
-(defvar cfg/default-font-size 95)
-(defvar cfg/default-variable-font-size 95)
-(defvar cfg/font "CaskaydiaCove Nerd Font")
-;;(defvar cfg/font "VictorMono Nerd Font Mono")
+(defvar tilman/default-font-size 95)
+(defvar tilman/default-variable-font-size 95)
+(defvar tilman/font "CaskaydiaCove Nerd Font")
+;;(defvar tilman/font "VictorMono Nerd Font Mono")
 
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1) ; disable scrollbar
@@ -10,6 +10,9 @@
 (tooltip-mode -1) ;; disable tooltip
 (set-fringe-mode 10) ;;set fringe to 10
 (menu-bar-mode -1) ; disable menu bar
+(setq use-short-answers t)
+(setq-default tab-width 2)
+
 
 (global-display-line-numbers-mode 1) ; enable line numbers
 (setq display-line-numbers-type 'relative)
@@ -26,9 +29,9 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
 ;; Font
-(set-face-attribute 'default nil :font cfg/font :height cfg/default-font-size :weight 'regular)
-(set-face-attribute 'fixed-pitch nil :font cfg/font :height cfg/default-font-size :weight 'regular)
-(set-face-attribute 'variable-pitch nil :font "Cantarell" :height cfg/default-variable-font-size :weight 'regular)
+(set-face-attribute 'default nil :font tilman/font :height tilman/default-font-size :weight 'regular)
+(set-face-attribute 'fixed-pitch nil :font tilman/font :height tilman/default-font-size :weight 'regular)
+(set-face-attribute 'variable-pitch nil :font "ETBembo" :height tilman/default-variable-font-size :weight 'regular)
 
 ;; Plugins
 (require 'package)
@@ -87,6 +90,8 @@
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-i-jump nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
   :config
   (define-key evil-normal-state-map (kbd "SPC f f") 'find-file)
   (define-key evil-normal-state-map (kbd "SPC f w") 'consult-ripgrep)
@@ -111,8 +116,70 @@
 ;; Keymaps
 (use-package general
   :ensure t
+	:config
+	(general-evil-setup t)
   :after evil)
 
+(general-define-key
+ :states '(normal motion)
+ :keymaps 'override
+ :prefix "SPC"
+
+ ;; Top level stuff
+ "/" '(consult-ripgrep :which-key "ripgrep")
+ "SPC" '(execute-extended-command :which-key "M-x")
+ "q" '(evil-quit :which-key "quit emacs")
+ "." '(find-files :which-key "find files")
+
+ ;; Files
+ "f" '(nil :which-key "files")
+ "ff" '(find-files :which-key "find files")
+ "fw" '(consult-ripgrep :which-key "ripgrep")
+ "fs" '(save-buffer :which-key "save file")
+ "fS" '(evil-write-all :which-key "save all buffer")
+ "fR" '(rename-buffer :which-key "rename file")
+
+ ;; window
+ "w" '(nil :which-key "window")
+ "wh" '(evil-window-left :which-key "switch to left split")
+ "wj" '(evil-window-down :which-key "switch to down split")
+ "wk" '(evil-window-up :which-key "switch to up split")
+ "wl" '(evil-window-right :which-key "switch to right split")
+ "wc" '(evil-window-delete :which-key "close current split")
+
+ ;; Buffer
+ "b" '(nil :which-key "buffer")
+ "bb" '(consult-buffer :which-key "switch buffer")
+ "bd" '(evil-delete-buffer :which-key "close current buffer")
+
+ ;; Help/emacs
+ "h" '(nil :which-key "help/emacs")
+ 
+ "hv" '(describe-variable :which-key "des. variable")
+ "hb" '(describe-bindings :which-key "des. bindings")
+ "hM" '(describe-mode :which-key "des. mode")
+ "hf" '(describe-function :which-key "des. func")
+ "hF" '(describe-face :which-key "des. face")
+ "hk" '(describe-key :which-key "des. key")
+ 
+ "hm" '(nil :which-key "switch mode")
+ "hme" '(emacs-lisp-mode :which-key "elisp mode")
+ "hmo" '(org-mode :which-key "org mode")
+ "hmt" '(text-mode :which-key "text mode")
+ 
+ "hp" '(nil :which-key "packages")
+ "hpr" 'package-refresh-contents
+ "hpi" 'package-install
+ "hpd" 'package-delete
+ 
+ ;; Toggles
+ "t" '(nil :which-key "toggles")
+ "tt" '(toggle-truncate-lines :which-key "truncate lines")
+ "tv" '(visual-line-mode :which-key "visual line mode")
+ "tn" '(display-line-numbers-mode :which-key "display line numbers")
+ "th" '(load-theme :which-key "load theme")
+)
+ 
 (use-package key-chord
   :ensure t)
 
@@ -120,6 +187,14 @@
 (use-package rainbow-delimiters
   :ensure t
   :hook (prog-mode))
+
+;; Scaling text
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+
+;; With mouse
+(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+(global-set-key (kbd "<-C-wheel-down>") 'text-scale-decrease)
 
 ;; Looks
 
@@ -173,9 +248,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "d6324e8a397986b032380cf076ed73d2097f3546caa9f554459b10769f55b8e7" "b89430305c7e352c4c9256caf8fcad39d01cdcc78e455b4a025df36e4b8b152f" "34db02351a45841c08369268d5cfa6b5ce49b5449e1c18c893666f57f8970cdd" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
+	 '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "d6324e8a397986b032380cf076ed73d2097f3546caa9f554459b10769f55b8e7" "b89430305c7e352c4c9256caf8fcad39d01cdcc78e455b4a025df36e4b8b152f" "34db02351a45841c08369268d5cfa6b5ce49b5449e1c18c893666f57f8970cdd" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
  '(package-selected-packages
-   '(key-chord general magit all-the-icons-dired all-the-icons doom-modeline doom-themes rainbow-delimiters evil-collection evil marginalia consult orderless vertico use-package)))
+	 '(key-chord general magit all-the-icons-dired all-the-icons doom-modeline doom-themes rainbow-delimiters evil-collection evil marginalia consult orderless vertico use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
