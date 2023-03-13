05533fcba60360c555ec2bbe7bc1286777a0182c
tilmanmix <uju8765@gmail.com>
2023-03-13 23:11:02 +0100

    updating garbage collection and native comp options in early-init.el

--- early-init.el
+++ early-init.el
@@ -1,1 +1,12 @@
+;; Set Garbage Collection threshold for faster startup
+(setq gc-cons-threshold (* 50 100 1000))
+
+;; Native Comp settings
+(when (featurep 'native-compile)
+  ;; Silence annyoing warning
+  (setq native-comp-async-report-warnings-error nil))
+;; Set correct Natice comp cache path
+(add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
+
+;; Disable package.el for elpaca to function
 (setq package-enable-at-startup nil)
