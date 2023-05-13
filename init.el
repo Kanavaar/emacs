472c3e778e0c6681c7a1a0a49e71e16dcdaf28be
Kanavaar <uju8765@gmail.com>
2023-05-13 16:27:04 +0200

    fix fore meow's of cursor parens highlighting

--- init.el
+++ init.el
@@ -168,6 +168,21 @@
   (electric-pair-mode 1)
   :elpaca nil)
 
+(show-paren-mode)
+(defun show-paren--locate-near-paren-ad ()
+  "Locate an unescaped paren \"near\" point to show.
+If one is found, return the cons (DIR . OUTSIDE), where DIR is 1
+for an open paren, -1 for a close paren, and OUTSIDE is the buffer
+position of the outside of the paren.  Otherwise return nil."
+  (let* ((before (show-paren--categorize-paren (point))))
+    (when (or
+           (eq (car before) 1)
+           (eq (car before) -1))
+      before)))
+
+(advice-add 'show-paren--locate-near-paren
+            :override #'show-paren--locate-near-paren-ad)
+
 (use-package rainbow-delimiters
   :elpaca t
   :hook (prog-mode . rainbow-delimiters-mode))
